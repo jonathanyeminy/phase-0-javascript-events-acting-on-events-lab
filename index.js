129ad614d76e21bb6850597aac44b0c61ebf95f6
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-03-21 10:54:13 -0400

    Done.

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ document.addEventListener("keydown", function (event) {
       const left = parseInt(leftNumbers, 10);
         
       if(left>0){
-      dodger.style.left = `${left - 1}px`;
+      dodger.style.left = `${left - 10}px`;
     }}
 
     function moveDodgerRight(){
         const leftNumbers = dodger.style.left.replace("px", "");
       const left = parseInt(leftNumbers, 10);
         
-      if(left<140){
-      dodger.style.left = `${left + 1}px`;
+      if(left<355){
+      dodger.style.left = `${left + 10}px`;
     }}
     
   
